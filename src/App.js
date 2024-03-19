40138b1848e6c9f1d17b4794ea4fc6e7975ffe24
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-19 03:40:41 +0100

    write code to delete book in App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import BookList from './components/BookList';
 
 function App (){
     const [books , setBooks] = useState([]);
+    const deleteBookById = (id)=>{
+     const updatedBooks = books.filter((book)=>{
+        return book.id !== id;
+    
+     });
+     setBooks(updatedBooks);
+    };
     const createBook = (title) =>{
         const updatedBooks = [
             ...books, {id: Math.round(Math.random()*9999)
@@ -19,7 +26,7 @@ function App (){
 
     };
     return <div className="app">
-       <BookList books={books} />
+       <BookList books={books} onDelete ={deleteBookById} />
         <BookCreate onCreate={createBook} />
 
     </div>;
