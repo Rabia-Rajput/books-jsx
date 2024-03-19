7ba27b9f2e0196f7adedd6281259d945193643b2
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-19 04:52:02 +0100

    write code to submit edited form in BookEdit.js

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
-function BookEdit({book}){
+function BookEdit({book,onEdit}){
     const [title , setTitle ]= useState(book.title);
     const handleChange = (event)=>{
         setTitle(event.target.value);
     };
     const handleSubmit =(event)=>{
         event.preventDefault();
-    }
+        onEdit(book.id,title);
+    };
    
     return <form onSubmit={handleSubmit} className="book-edit">
          <label>Title</label>
