4d92661063656d98c934363b6d36c5628a97ef73
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-19 03:53:23 +0100

    write code for book editing

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
 function BookShow({book, onDelete}){
-    const handleClick=()=>{
+    const [showEdit , setShowEdit]= useState(false);
+    const handleDeleteClick=()=>{
         onDelete(book.id);
+    };
+    const handleEditClick = () =>{
+
     };
 
     return <div className="book-show">
         {book.title}
         <div className="actions">
-            <button className="delete" onClick={handleClick}>Delete</button>
+            <button className="edit" onClick={handleEditClick}>Edit</button>
+            <button className="delete" onClick={handleDeleteClick}>Delete</button>
         </div>
         </div>;
 }
