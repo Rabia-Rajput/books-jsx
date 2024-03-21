056c8f84665a0965d4eb61245ba71f7de476ac52
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-21 01:47:23 +0100

    add context code in index.js file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import './index.css';
 import React from "react";
 import ReactDom from 'react-dom/client';
 import App from './App';
+import BooksContext from './context/books';
 
 const el = document.getElementById('root');
 const root = ReactDom.createRoot(el);
 
-root.render(<App/> );
+root.render( <BooksContext.Provider value={5}>
+    <App/> 
+</BooksContext.Provider> );
