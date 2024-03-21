edf7fc30b25011ada6fa4eed2af2897bcf629ab7
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-21 01:01:09 +0100

    code for updating books in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from './components/BookList';
@@ -10,10 +10,21 @@ import BookList from './components/BookList';
 
 function App (){
     const [books , setBooks] = useState([]);
-    const editBookById = (id, newTitle)=>{
+    const fetchBooks = async()=>{
+        const response= await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+       };
+
+       useEffect(() => {
+    fetchBooks();
+       },[] );
+    const editBookById =async (id, newTitle)=>{
+        const response = await axios.put(`http://localhost:3001/books/${id}`,{
+            title: newTitle
+        });
 const updatedBooks = books.map((book) => {
     if(book.id===id){
-        return{...book, title:newTitle};
+        return{...book, ...response.data};// response.data is updated book object came back from the api
 
     }
     return book;
@@ -21,7 +32,9 @@ const updatedBooks = books.map((book) => {
 setBooks(updatedBooks);
 
     };
-    const deleteBookById = (id)=>{
+    const deleteBookById =async (id)=>{
+        await axios.delete(`http://localhost:3001/books/${id}`);
+        
      const updatedBooks = books.filter((book)=>{
         return book.id !== id;
     
@@ -43,6 +56,7 @@ setBooks(updatedBooks);
 
 
     };
+
     return <div className="app">
         <h1>Reading List</h1>
        <BookList onEdit={editBookById} books={books} onDelete ={deleteBookById} />
