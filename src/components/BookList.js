91e5e17158df60f5e8bec09941be7a4d1beac6fc
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-24 21:46:43 +0100

    refactor code in BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,13 @@
-import { useContext } from 'react';
-import BooksContext from '../context/books';
+
 import BookShow from './BookShow';
+import useBooksContext from '../hooks/use-books-context';
+
+// function useBooksContext (){
+//     return useContext(BooksContext);
+// }
 function BookList(){
-    const {books} = useContext(BooksContext)
-    // const {count, incrementCount} = useContext(BooksContext);
-    const renderedBooks = books.map((book)=>{
+    const {books} = useBooksContext();
+    const renderedBooks = books.map((book) =>{
         return <BookShow  key={book.id} book={book}/>;
     });
     return <div className='book-list'>
