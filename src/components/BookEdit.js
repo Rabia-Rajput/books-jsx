34a7863be318407413ce815ca9134e4e0b01a1d9
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-24 20:42:43 +0100

    replace props code as cotext in BookEdit.js

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState,useContext } from "react";
+import BooksContexts from "../context/books";
 function BookEdit({book,onSubmit}){
     const [title , setTitle ]= useState(book.title);
+    const {editBookById}= useContext(BooksContexts);
     const handleChange = (event)=>{
         setTitle(event.target.value);
     };
     const handleSubmit =(event)=>{
         event.preventDefault();
-        onSubmit(book.id,title);
+        onSubmit();
+        editBookById(book.id, title);
     };
    
     return <form onSubmit={handleSubmit} className="book-edit">
